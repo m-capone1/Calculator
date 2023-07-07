3db76981bb3a4555b4fb7353b3eb25e2eb7136c1
m-capone1 <110258992+m-capone1@users.noreply.github.com>
2023-07-07 14:18:32 -0300

    Create operator functions and variables

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function add(a, b) {
+  return a + b;
+}
 
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+let num1 = 0;
+let num2 = 0;
+let operator = '+';
+
+function operate(num1, num2, operator){
+
+}
