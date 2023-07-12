a2cc6c9ccb59e58477ed4ec0bb2bbc523724a7aa
m-capone1 <110258992+m-capone1@users.noreply.github.com>
2023-07-12 15:20:58 -0300

    Add operate function

--- script.js
+++ script.js
@@ -18,6 +18,15 @@ let num1 = 0;
 let num2 = 0;
 let operator = '+';
 
-function operate(num1, num2, operator){
-
+function operate(num1, num2, operator) {
+  if (operator === "+") {
+    add(num1, num2);
+  } else if (operator === "-") {
+    subtract(num1, num2);
+  } else if (operator === "/") {
+    divide(num1, num2);
+  } else if (operator === "x") {
+    divide(num1, num2);
+  } else
+    return;
 }
